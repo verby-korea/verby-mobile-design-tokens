f55efecd073273604ed5e2a6c60fc9463e7f53fd
Bigstar1108 <daesung09854@gmail.com>
2022-10-01 16:51:55 +0900

    [Fix]: fix palette

---
+++ lib/colors/palette/extensions/blue_palette_extension.dart
@@ -0,0 +1,33 @@
+part of palette;
+
+extension BluePaletteExtension on Palette {
+  /// **blue5**: `#E2F1FA`
+  static const Palette blue5 = Palette._(0XFFE2F1FA);
+
+  /// **blue10**: `#B9DCF5`
+  static const Palette blue10 = Palette._(0XFFB9DCF5);
+
+  /// **blue20**: `#8EC6EF`
+  static const Palette blue20 = Palette._(0XFF8EC6EF);
+
+  /// **blue30**: `#62B0E8`
+  static const Palette blue30 = Palette._(0XFF62B0E8);
+
+  /// **blue40**: `#409FE4`
+  static const Palette blue40 = Palette._(0XFF409FE4);
+
+  /// **blue50**: `#1C90E0`
+  static const Palette blue50 = Palette._(0XFF1C90E0);
+
+  /// **blue60**: `#1482D3`
+  static const Palette blue60 = Palette._(0XFF1482D3);
+
+  /// **blue70**: `#0871C1`
+  static const Palette blue70 = Palette._(0XFF0871C1);
+
+  /// **blue80**: `#0060AF`
+  static const Palette blue80 = Palette._(0XFF0060AF);
+
+  /// **blue90**: `#004391`
+  static const Palette blue90 = Palette._(0XFF004391);
+}
