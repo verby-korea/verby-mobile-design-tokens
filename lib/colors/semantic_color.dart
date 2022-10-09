eda45d5075f23f2297d67fa30aaad1b5445a1de8
Bigstar1108 <daesung09854@gmail.com>
2022-10-09 23:00:05 +0900

    [Fix]: migrate to Palette

--- lib/colors/semantic_color.dart
+++ lib/colors/semantic_color.dart
@@ -13,41 +13,49 @@ class SemanticColor {
   const SemanticColor._(this.palette);
 
   // >>> Main >>>
-  /// **main90**: BluePaletteExtension.blue90
-  static const SemanticColor main90 = SemanticColor._(BluePaletteExtension.blue90);
+  /// **main90**: Palette.blue90
+  static const SemanticColor main90 = SemanticColor._(Palette.blue90);
 
-  /// **main70**: BluePaletteExtension.blue70
-  static const SemanticColor main70 = SemanticColor._(BluePaletteExtension.blue70);
+  /// **main70**: Palette.blue70
+  static const SemanticColor main70 = SemanticColor._(Palette.blue70);
   // <<< Main <<<
 
   // >>> Sub Caution >>>
-  /// **subCaution**: RedPaletteExtension.red80
-  static const SemanticColor subCaution = SemanticColor._(RedPaletteExtension.red80);
+  /// **subCaution**: Palette.red80
+  static const SemanticColor subCaution = SemanticColor._(Palette.red80);
   // <<< Sub Caution <<<
 
   // >>> Text >>>
-  /// **text90**: GrayPaletteExtension.gray90
-  static const SemanticColor text90 = SemanticColor._(GrayPaletteExtension.gray90);
+  /// **text90**: Palette.gray90
+  static const SemanticColor text90 = SemanticColor._(Palette.gray90);
 
-  /// **text50**: GrayPaletteExtension.gray50
-  static const SemanticColor text50 = SemanticColor._(GrayPaletteExtension.gray50);
+  /// **text50**: Palette.gray50
+  static const SemanticColor text50 = SemanticColor._(Palette.gray50);
 
-  /// **text30**: GrayPaletteExtension.gray30
-  static const SemanticColor text30 = SemanticColor._(GrayPaletteExtension.gray30);
+  /// **text30**: Palette.gray30
+  static const SemanticColor text30 = SemanticColor._(Palette.gray30);
 
-  /// **text10**: DefaultPaletteExtension.white
-  static const SemanticColor text10 = SemanticColor._(DefaultPaletteExtension.white);
+  /// **text10**: Palette.white
+  static const SemanticColor text10 = SemanticColor._(Palette.white);
   // <<< Text <<<
 
   // >>> Background >>>
-  /// **background20**: GrayPaletteExtension.gray20
-  static const SemanticColor background20 = SemanticColor._(GrayPaletteExtension.gray20);
+  /// **background20**: Palette.gray20
+  static const SemanticColor background20 = SemanticColor._(Palette.gray20);
 
-  /// **background10**: DefaultPaletteExtension.white
-  static const SemanticColor background10 = SemanticColor._(DefaultPaletteExtension.white);
+  /// **background10**: Palette.white
+  static const SemanticColor background10 = SemanticColor._(Palette.white);
   // <<< Background <<<
 
-  // >>> Default >>>
-  static const SemanticColor transparent = SemanticColor._(DefaultPaletteExtension.transparent);
-  // <<< Default <<<
+  // >>> Border >>>
+  /// **border40**: Palette.gray40
+  static const SemanticColor border40 = SemanticColor._(Palette.gray40);
+  // <<< Border <<<
+
+  // >>> Dim >>>
+  /// **dim50**: Palette.gray50.withOpacity(0.5)
+  static final SemanticColor dim50 = SemanticColor._(
+    Palette.colorToPalette(Palette.gray50.withOpacity(0.5).value),
+  );
+  // <<< Dim <<<
 }
